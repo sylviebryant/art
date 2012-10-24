749573f60ac6a1c048ff77f1ea2fae73c610e1d8
Ruoxing Liu <liurosin@gmail.com>
2012-10-23 17:43:35 -0700

    added save/print/share page and share page

---
+++ final-edit.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<title>Save & Share page</title> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+</head> 
+<body> 
+
+<div data-role="page">
+
+	<div data-role="header">
+		<h1>Almost done!</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">	
+		<p>This page saves, prints and shares the painting</p>		
+	</div><!-- /content -->
+
+	<a href="index.html" data-role="button">Save</a>
+	<a href="index.html" data-role="button">Print</a>
+	<a href="share-page.php" data-role="button">Share</a>
+	<a href="index.html" data-role="button" data-inline="true" data-theme="a">Back</a>
+	<a href="index.html" data-role="button" data-inline="true">Done</a>
+
+
+</div><!-- /page -->
+
+</body>
+</html>
