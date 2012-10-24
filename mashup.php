fc407830b575a7a0d39e94d9cc7255619f875a50
Ali Fauci <afauci@stanford.edu>
2012-10-24 13:46:12 -0700

    Added my files

---
+++ mashup.php
@@ -0,0 +1,20 @@
+<div class="backbutton">
+	<a href="index.php" data-rel="back" class="backbutton">Back</a>
+	<a href="home.php" class="homebutton">Home</a>
+</div>
+<div class="pretty">
+<a href="selection.php">
+<img width="140" height="140" src='photo1.png'/>
+</a>
+<a href="">
+<img width="140" height="140" src='photo2.png' />
+</a>
+</div>
+<div class="pretty">
+<a href="selection.php">
+<img width="140" height="140" src='light-dots.png' />
+</a>
+<a href="selection.php">
+<img width="140" height="140" src='water.png' />
+</a>
+</div>
---
+++ pick.php
@@ -0,0 +1,12 @@
+<div>
+<a href="home.php" data-rel="back" class="backbutton">Back</a>
+</div>
+<div class="box box1">
+	<a href="" class="coda">Pick Photo 1</a>
+</div>
+<div class="box box1">
+	<a href="" class="coda">Pick Photo 2</a>
+</div>
+<div class="box box2">
+<a href="mashup.php" class="coda" data-transition="flip">Next</a>
+</div>
