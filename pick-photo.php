7e7151bcf7c550e50d24e94918520da6d84b99f4
Ali Fauci <afauci@stanford.edu>
2012-10-24 19:19:14 -0700

    Added pick-photo instead of index2 calls

---
+++ pick-photo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<title>Pick Photos</title> 
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+</head> 
+
+<body> 
+<div data-role="controlgroup" data-type="horizontal">
+<a href="take-photo.php" data-role="button">Pick Photo 1</a>
+<a href="take-photo.php" data-role="button">Pick Photo 2</a>
+</div>
+<a href="mashup.php" data-role="button">Generate</a>
+</body>
+</html>
