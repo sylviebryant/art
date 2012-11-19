4c08de8ac0296f36bcb6c4076856ac85bd709621
Sylvie <sylviebryant@gmail.com>
2012-11-19 14:59:47 -0800

    Added icon and startup image

--- login.php
+++ login.php
@@ -28,10 +28,39 @@
   
 <head> 
 	<title>Login</title>
+	<!-- 
+Ours:
+
+From week5 VoteCaster:	
+	<title>VoteCasterFail</title> 
+	<meta charset="utf-8">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+ 	<meta name="apple-mobile-web-app-status-bar-style" content="purple">
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+
+	<link rel="stylesheet" href="jquery.mobile-1.2.0.css" />
+	<link rel="stylesheet" href="style.css" />
+	<link rel="apple-touch-icon" href="appicon.png" />
+	<link rel="apple-touch-startup-image" href="startup.png">
+	
+	<script src="jquery-1.8.2.min.js"></script>
+	<script src="jquery.mobile-1.2.0.js"></script>
+-->
+
+<meta charset="utf-8">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+ 	<meta name="apple-mobile-web-app-status-bar-style" content="purple">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+	<link rel="stylesheet" href="jquery.mobile-1.2.0.css" />
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />	
+
+	<link rel="apple-touch-icon" href="Mash-Photato-Icon.png" />
+	<link rel="apple-touch-startup-image" href="mashed-potatoes-and-gravy.png">
+	
 	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+
 </head> 
 
 <body>
